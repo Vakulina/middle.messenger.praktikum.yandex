687b68dcda805b2b2c3c8ef8064caaf251c34955
Victoria Yakovleva <v.yakovleva@inostudio.net>
2023-02-04 03:33:30 +0300

    fix export error

--- src/components/chatSidebar/ChatSidebar.ts
+++ src/components/chatSidebar/ChatSidebar.ts
@@ -1,7 +1,7 @@
 import tpl from './tpl.hbs';
 import s from './style.module.scss';
 import Block from '~src/services/Block';
-import { Panel } from './panel';
+import { Panel } from './controlPanel';
 
 interface ChatSidebarProps {
   items: Block[],
