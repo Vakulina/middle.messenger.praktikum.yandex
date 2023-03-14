37eefce6856ab8b2f4e8337a37941a5d6cfae80e
Victoria Yakovleva <v.yakovleva@inostudio.net>
2023-03-15 01:26:34 +0300

    fix error of import/no-cycle

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14
+FROM node:16.17.0
 
 COPY . .
 RUN npm install
--- src/modules/serverErrorPage/ServerErrorPage.ts
+++ src/modules/serverErrorPage/ServerErrorPage.ts
@@ -4,4 +4,4 @@ import { serverError } from '../../components/ServerError';
 const error = serverError({ code: '404', message: 'Не туда попали' });
 
 const getServerErrorPage = () => new PageLayout({ content: error });
-export default getServerErrorPage;
+export { getServerErrorPage };
