d007976004bbaeaf2a86f45399ed3d7956e1852e
Victoria Yakovleva <v.yakovleva@inostudio.net>
2023-03-14 13:48:58 +0300

    add HTTPTransport.test

--- src/services/Router.test.ts
+++ src/services/Router.test.ts
@@ -3,29 +3,22 @@ import { expect } from 'chai';
 import Block from './Block';
 import Route from './Route'
 
-describe.only('Router', () => {
-
-
-  const BlockMock = class {
-
-  } as unknown as Block;
-
+describe('Router', () => {
+  const BlockMock = class {} as unknown as Block;
 
   it('use() возвращает instance класса Router(т.к. используется паттерн singlton) ', () => {
     const result = router.use('/', BlockMock);
     expect(result).to.eq(router);
   });
 
-
-  
-  it('getRoute возвращает существующий роут', () => {
+  it('getRoute() возвращает существующий роут', () => {
     router.use('/logout', BlockMock);
     const existedRoute = router.getRoute('/logout');
     expect(existedRoute instanceof Route).to.eq(true);
   });
+
   it('go() добавляет запись в историю', () => {
     router.go('/login');
     expect(window.location.pathname).to.eq('/login');
   });
-
 });
