c55f5547ea694d2782b7a3a2c7aaf64f6f514530
Victoria Yakovleva <v.yakovleva@inostudio.net>
2023-03-14 22:24:57 +0300

    fix import

--- src/modules/notFoundPage/NotFoundPage.ts
+++ src/modules/notFoundPage/NotFoundPage.ts
@@ -4,4 +4,4 @@ import { serverError } from '../../components/ServerError';
 const error = serverError({ code: '404', message: 'Не туда попали' });
 
 const getNotFoundPage = () => new PageLayout({ content: error });
-export default getNotFoundPage
+export default getNotFoundPage;
